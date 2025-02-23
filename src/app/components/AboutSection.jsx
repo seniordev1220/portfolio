d5f24bd60cd53ee36410e3ad143db43f3e39763a
srfullstack <srfullstack3627@gmail.com>
2025-02-23 10:56:18 -0500

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,108 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <div className="flex gap-4">
+        <div className="">
+          <li>React</li>
+          <li>Next.js</li>
+          <li>Python</li>
+          <li>Django</li>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+        </div>
+        <div className="">
+          <li>React Native</li>
+          <li>Flutter</li>
+          <li>Java</li>
+          <li>Kotlin</li>
+          <li>AI Development</li>
+          <li>Cloud Computing</li>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Fullstack Academy of Code</li>
+        <li>The University of Texas at Dallas</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          I am a highly skilled and versatile developer with expertise in modern web and mobile development technologies. 
+          My core skills include React, Next.js, React Native, and back-end development using Python and Django. 
+          I also have experience in AI development and mobile app development with Java and Kotlin, Flutter. 
+          My diverse skill set allows me to build robust, scalable, and innovative solutions across various platforms. 
+          I am passionate about staying updated with the latest technologies and delivering high-quality products.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
